87ce009ec5dbfec9e69abb2b84ce8c21a35f9226
Manthan Gupta <manthangupta109@gmail.com>
2020-11-06 21:35:56 +0530

    Github Link added

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -69,7 +69,7 @@ const Hero = ({
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="">
+                  <Button tag="a" color="primary" wideMobile href="https://github.com/Manthan109/fitMe">
                     View On Github
                     </Button>
 
