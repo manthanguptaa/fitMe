222e623b77f28c6f45a44f991fb76d8601280d78
Manthan Gupta <manthangupta109@gmail.com>
2020-11-06 22:51:44 +0530

    Signup UI

---
+++ src/components/signup.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Form, Button, Card } from 'react-bootstrap'
+
+export default function signup() {
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4">
+                        Sign Up
+                    </h2>
+                    <Form>
+                        <Form.Group id="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" ref={emailRef} required />
+                        </Form.Group>
+                        <Form.Group id="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" ref={passwordRef} required />
+                        </Form.Group>
+                        <Form.Group id="password-confirm">
+                            <Form.Label>Password Confirmation</Form.Label>
+                            <Form.Control type="password" ref={passwordConfirmRef} required />
+                        </Form.Group>
+                        <Button className="w-100" type="Submit">Sign Up</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-2">
+                Already have an account? Log In
+            </div>
+        </>
+    )
+}
