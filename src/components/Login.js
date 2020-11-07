1d641ab7e56046d9386f6da75aad7f6c5c02482a
Manthan Gupta <manthangupta109@gmail.com>
2020-11-07 13:47:18 +0530

    Routes fixed

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,7 +19,7 @@ export default function Login() {
             setError("")
             setLoading(true)
             await login(emailRef.current.value, passwordRef.current.value)
-            history.push("/")
+            history.push("/dashbord")
         } catch {
             setError("Failed to log in")
         }
--- src/components/signup.js
+++ src/components/signup.js
@@ -24,7 +24,7 @@ export default function Signup() {
             setError("")
             setLoading(true)
             await signup(emailRef.current.value, passwordRef.current.value)
-            history.push("/")
+            history.push("/dashboard")
         } catch {
             setError("Failed to create an account")
         }
